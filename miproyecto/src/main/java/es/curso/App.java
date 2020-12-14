3b027a3a837fc6df55a438d5834022e04029094c
Ubuntu <ubuntu@ip-172-31-3-49.eu-west-1.compute.internal>
2020-12-14 18:49:34 +0000

    pruebas

--- miproyecto/src/main/java/es/curso/App.java
+++ miproyecto/src/main/java/es/curso/App.java
@@ -11,12 +11,12 @@ public class App
         System.out.println( "Hola majo!" );
     }
 
-     public static void doblar( int entero )
+     public static int doblar( int entero )
     {
         return numero*2;
     }
     
-     public static void mayor( int numero1, numero2 )
+     public static int mayor( int numero1, numero2 )
     {
         if (numero1 > numero2){
         return numero1
